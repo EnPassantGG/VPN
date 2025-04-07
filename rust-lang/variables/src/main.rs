e4221fd645f25feb9bb08fd04333ca3958267248
Jacob <ll1bytecode@gmail.com>
2025-04-06 22:57:50 -0500

    Learn shadowing. Can use same name for new variables with "let example_name" again and again.

--- rust-lang/variables/src/main.rs
+++ rust-lang/variables/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // Constants
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
@@ -11,4 +12,14 @@ fn main() {
     let radius = 3f32;
     let surface_area = PIE * radius * radius;
     println!("Surface area of circle with r=3: {surface_area}");
+
+    // Shadowing
+    // (Super cool, and will improve readability)0
+    let y = 5;
+    let y = y + 1;
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {y}");
+    }
+    println!("The value of y is: {y}");
 }
