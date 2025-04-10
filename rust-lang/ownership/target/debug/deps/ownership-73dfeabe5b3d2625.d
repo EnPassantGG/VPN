bf5137f3a1e5f8a6ae79866eb331f632cf6e68af
Jacob <ll1bytecode@gmail.com>
2025-04-10 06:16:28 -0500

    Learn the fundamental Rust concept of borrowing. Learn how & works under the hood.

--- rust-lang/ownership/target/debug/deps/ownership-73dfeabe5b3d2625.d
+++ rust-lang/ownership/target/debug/deps/ownership-73dfeabe5b3d2625.d
@@ -1,5 +1,6 @@
-/mnt/c/Users/hallj/OneDrive/Desktop/website/VPN/rust-lang/ownership/target/debug/deps/libownership-73dfeabe5b3d2625.rmeta: src/main.rs
+/mnt/c/Users/hallj/OneDrive/Desktop/website/VPN/rust-lang/ownership/target/debug/deps/libownership-73dfeabe5b3d2625.rmeta: src/main.rs src/functions.rs
 
-/mnt/c/Users/hallj/OneDrive/Desktop/website/VPN/rust-lang/ownership/target/debug/deps/ownership-73dfeabe5b3d2625.d: src/main.rs
+/mnt/c/Users/hallj/OneDrive/Desktop/website/VPN/rust-lang/ownership/target/debug/deps/ownership-73dfeabe5b3d2625.d: src/main.rs src/functions.rs
 
 src/main.rs:
+src/functions.rs:
--- rust-lang/ownership/target/debug/deps/ownership-904f37c96524ed17.d
+++ rust-lang/ownership/target/debug/deps/ownership-904f37c96524ed17.d
@@ -1,5 +1,6 @@
-/mnt/c/Users/hallj/OneDrive/Desktop/website/VPN/rust-lang/ownership/target/debug/deps/libownership-904f37c96524ed17.rmeta: src/main.rs
+/mnt/c/Users/hallj/OneDrive/Desktop/website/VPN/rust-lang/ownership/target/debug/deps/libownership-904f37c96524ed17.rmeta: src/main.rs src/functions.rs
 
-/mnt/c/Users/hallj/OneDrive/Desktop/website/VPN/rust-lang/ownership/target/debug/deps/ownership-904f37c96524ed17.d: src/main.rs
+/mnt/c/Users/hallj/OneDrive/Desktop/website/VPN/rust-lang/ownership/target/debug/deps/ownership-904f37c96524ed17.d: src/main.rs src/functions.rs
 
 src/main.rs:
+src/functions.rs:
