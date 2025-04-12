645b4942bed5a113e2df134b422a1958a3935037
Jacob <ll1bytecode@gmail.com>
2025-04-11 23:08:27 -0500

    Add better function that returns a slice. This is superior because you return a new String so now you don't need to keep track of the index and original String.

--- rust-lang/references/target/debug/deps/references-f03378129595502a.d
+++ rust-lang/references/target/debug/deps/references-f03378129595502a.d
@@ -1,5 +1,7 @@
-/mnt/c/Users/hallj/OneDrive/Desktop/website/VPN/rust-lang/references/target/debug/deps/libreferences-f03378129595502a.rmeta: src/main.rs
+/mnt/c/Users/hallj/OneDrive/Desktop/website/VPN/rust-lang/references/target/debug/deps/libreferences-f03378129595502a.rmeta: src/main.rs src/immutable_reference.rs src/dangling_references.rs
 
-/mnt/c/Users/hallj/OneDrive/Desktop/website/VPN/rust-lang/references/target/debug/deps/references-f03378129595502a.d: src/main.rs
+/mnt/c/Users/hallj/OneDrive/Desktop/website/VPN/rust-lang/references/target/debug/deps/references-f03378129595502a.d: src/main.rs src/immutable_reference.rs src/dangling_references.rs
 
 src/main.rs:
+src/immutable_reference.rs:
+src/dangling_references.rs:
--- rust-lang/slice/src/main.rs
+++ rust-lang/slice/src/main.rs
@@ -4,14 +4,31 @@ fn main() {
     println!("Word: {}, Function: {}", word, len);
 }
 
+// We can be more efficient by returning a slice, thereby 'binding'
+// the String to the usize variable so that they're always together
 fn first_word(s: &String) -> usize {
+    // Check the String char by char
     let bytes = s.as_bytes();
 
+    // Create an iterator over the array
     for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
+        if item == b' ' { // byte literal syntax for space
             return i;
         }
     }
 
     s.len()
+}
+
+fn first_word_slice(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i]; // now we don't need to keep track of the
+                            // index and original String
+        }
+    }
+
+    &s[..]
 }
